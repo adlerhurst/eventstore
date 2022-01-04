1a675fd5ede130112a52021f946c5b32b7b9e5a6
adlerhurst <silvan.reusser@gmail.com>
2022-01-04 09:14:08 +0100

    cleanup tests, add benchmarks

--- eventstore_test.go
+++ eventstore_test.go
@@ -2,7 +2,9 @@ package eventstore_test
 
 import (
 	"context"
-	"fmt"
+	"encoding/json"
+	"reflect"
+	"strconv"
 	"testing"
 
 	"github.com/adlerhurst/eventstore"
@@ -14,6 +16,13 @@ type testUser struct {
 	firstName string
 	lastName  string
 	username  string
+}
+
+var defaultTestUser = &testUser{
+	id:        "id",
+	firstName: "firstName",
+	lastName:  "lastName",
+	username:  "adlerhurst",
 }
 
 func (u testUser) toAdded() *testUserAdded {
@@ -136,24 +145,223 @@ func (e *testUserRemoved) Subjects() []eventstore.TextSubject {
 
 func (e *testUserRemoved) ResourceOwner() string { return "ro" }
 
-func (e *testUserRemoved) Payload() interface{} { return e }
+func (e *testUserRemoved) Payload() interface{} { return nil }
 
-func TestPush(t *testing.T) {
-	es := eventstore.New(memory.New())
-	user := testUser{
-		id:        "1",
-		firstName: "silvan",
-		lastName:  "reusser",
-		username:  "adlerhurst",
+func TestEventstore_Push(t *testing.T) {
+	type fields struct {
+		storage eventstore.Storage
 	}
-	es.Push(context.Background(),
-		user.toAdded(),
-		user.toRemoved(),
-	)
-	user.id = "2"
-	es.Push(context.Background(),
-		user.toAdded(),
-	)
-	events, err := es.Filter(context.Background(), eventstore.TextSubject("user"), eventstore.TextSubject("2"), eventstore.MultiToken)
-	fmt.Println(events, err)
+	type args struct {
+		commands []eventstore.Command
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    []eventstore.Event
+		wantErr bool
+	}{
+		{
+			name: "multiple events",
+			fields: fields{
+				storage: memory.New(),
+			},
+			args: args{
+				commands: []eventstore.Command{
+					defaultTestUser.toAdded(),
+					defaultTestUser.toRemoved(),
+				},
+			},
+			want: []eventstore.Event{
+				{
+					EditorService: "svc",
+					EditorUser:    "usr",
+					ResourceOwner: "ro",
+					Subjects:      []eventstore.TextSubject{"user", "id", "added"},
+					Sequence:      1,
+					Payload:       mustJSON(t, defaultTestUser.toAdded()),
+				},
+				{
+					EditorService: "svc",
+					EditorUser:    "usr",
+					ResourceOwner: "ro",
+					Subjects:      []eventstore.TextSubject{"user", "id", "removed"},
+					Sequence:      2,
+				},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			es := eventstore.New(tt.fields.storage)
+			got, err := es.Push(context.Background(), tt.args.commands...)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Eventstore.Push() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			for i, wanted := range tt.want {
+				if !reflect.DeepEqual(got[i], wanted) {
+					t.Errorf("Eventstore.Push() %d = %v, want %v", i, got[i], wanted)
+				}
+			}
+		})
+	}
+}
+
+func BenchmarkEventstorePush(b *testing.B) {
+	tests := []struct {
+		name    string
+		storage eventstore.Storage
+	}{
+		{
+			name:    "memory",
+			storage: memory.New(),
+		},
+	}
+	for _, tt := range tests {
+		b.Run(tt.name, func(b *testing.B) {
+			es := eventstore.New(tt.storage)
+			for n := 0; n < b.N; n++ {
+				user := new(testUser)
+				*user = *defaultTestUser
+				user.id = strconv.Itoa(n)
+				cmds := []eventstore.Command{
+					user.toAdded(),
+					user.toRemoved(),
+				}
+				_, err := es.Push(context.Background(), cmds...)
+				if err != nil {
+					b.Error(err)
+				}
+			}
+		})
+	}
+}
+
+func TestEventstore_Filter(t *testing.T) {
+	type fields struct {
+		storage eventstore.Storage
+	}
+	type args struct {
+		filter eventstore.Filter
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    []eventstore.Event
+		wantErr bool
+	}{
+		{
+			name: "multiple events",
+			fields: fields{
+				storage: memory.New(),
+			},
+			args: args{
+				filter: eventstore.Filter{
+					Subjects: []eventstore.Subject{eventstore.TextSubject("user"), eventstore.TextSubject("id"), eventstore.MultiToken},
+				},
+			},
+			want: []eventstore.Event{
+				{
+					EditorService: "svc",
+					EditorUser:    "usr",
+					ResourceOwner: "ro",
+					Subjects:      []eventstore.TextSubject{"user", "id", "added"},
+					Sequence:      1,
+					Payload:       mustJSON(t, defaultTestUser.toAdded()),
+				},
+				{
+					EditorService: "svc",
+					EditorUser:    "usr",
+					ResourceOwner: "ro",
+					Subjects:      []eventstore.TextSubject{"user", "id", "removed"},
+					Sequence:      2,
+				},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		ctx := context.Background()
+		cmds := []eventstore.Command{
+			defaultTestUser.toAdded(),
+			defaultTestUser.toRemoved(),
+		}
+		t.Run(tt.name, func(t *testing.T) {
+			es := eventstore.New(tt.fields.storage)
+			_, err := es.Push(ctx, cmds...)
+			if err != nil {
+				t.Fatalf("unable to push events: %v", err)
+			}
+			got, err := es.Filter(ctx, tt.args.filter)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Eventstore.Filter() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if len(tt.want) != len(got) {
+				t.Errorf("unexpected length of filtered events want %d, got %d", len(tt.want), len(got))
+				return
+			}
+			for i, wanted := range tt.want {
+				if !reflect.DeepEqual(got[i], wanted) {
+					t.Errorf("Eventstore.Filter() %d = %v, want %v", i, got[i], wanted)
+				}
+			}
+		})
+	}
+}
+
+func BenchmarkEventstoreFilter(b *testing.B) {
+	tests := []struct {
+		name    string
+		storage eventstore.Storage
+	}{
+		{
+			name:    "memory",
+			storage: memory.New(),
+		},
+	}
+	for _, tt := range tests {
+		b.Run(tt.name, func(b *testing.B) {
+			es := eventstore.New(tt.storage)
+			user := new(testUser)
+			*user = *defaultTestUser
+			user.id = "2"
+			cmds := []eventstore.Command{
+				user.toAdded(),
+				defaultTestUser.toAdded(),
+				defaultTestUser.toRemoved(),
+				user.toRemoved(),
+			}
+			_, err := es.Push(context.Background(), cmds...)
+			if err != nil {
+				b.Error(err)
+				b.FailNow()
+			}
+			for n := 0; n < b.N; n++ {
+				events, err := es.Filter(context.Background(), eventstore.Filter{
+					From:     1,
+					Limit:    2,
+					Subjects: []eventstore.Subject{eventstore.TextSubject("user"), eventstore.SingleToken, eventstore.TextSubject("added")},
+				})
+				if err != nil {
+					b.Error(err)
+				}
+				if len(events) != 2 {
+					b.Errorf("%d: 2 events should be returned got %d", n, len(events))
+				}
+			}
+		})
+	}
+}
+
+func mustJSON(t *testing.T, object interface{}) []byte {
+	t.Helper()
+	data, err := json.Marshal(object)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return data
 }
